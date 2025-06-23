82838ca126d2649046bfbe64096fe4286f8cb7c5
valera <valeragordienko_05012003@mail.ru>
2025-06-23 11:54:59 +0300

    Add CORS configuration and middleware for GitHub Pages integration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,9 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        //
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
